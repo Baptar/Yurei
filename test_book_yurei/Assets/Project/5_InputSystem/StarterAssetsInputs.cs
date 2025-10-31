3cbf2c885506e437b402b438b94f9195c3f0a304
Baptiste Ruberte <ruberte43@gmail.com>
2025-10-31 21:43:19 +0100

    clean

--- test_book_yurei/Assets/Project/5_InputSystem/StarterAssetsInputs.cs
+++ test_book_yurei/Assets/Project/5_InputSystem/StarterAssetsInputs.cs
@@ -12,6 +12,8 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+		public bool lookBook;
+		public bool exitBook;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -43,6 +45,16 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+		
+		public void OnLookBook(InputValue value)
+		{
+			LookBookInput(value.isPressed);
+		}
+		
+		public void OnExitBook(InputValue value)
+		{
+			ExitBookInput(value.isPressed);
+		}
 #endif
 
 
@@ -64,6 +76,16 @@ namespace StarterAssets
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
+		}
+		
+		public void LookBookInput(bool newSprintState)
+		{
+			lookBook = newSprintState;
+		}
+		
+		public void ExitBookInput(bool newSprintState)
+		{
+			exitBook = newSprintState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
