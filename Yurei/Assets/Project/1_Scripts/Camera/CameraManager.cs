e1bcfa4a465a3dc0511eae4edb05389813a32d0f
Baptiste Ruberte <ruberte43@gmail.com>
2025-11-04 15:47:37 +0100

    dqzd

--- Yurei/Assets/Project/1_Scripts/Camera/CameraManager.cs
+++ Yurei/Assets/Project/1_Scripts/Camera/CameraManager.cs
@@ -21,7 +21,7 @@ public class CameraManager : MonoBehaviour
     public void SwitchTo(Camera newCam, bool followPlayer = false, Vector3 zonePosition = default(Vector3), Ease easeFunction = Ease.InOutExpo, float easeDuration = 0.5f)
     {
         if (newCam == CurrentCamera) return;
-
+        
         // set the new and previous camera
         // we store previous camera in order to NOT deactivate element associated immediately when we leave the zone but when we enter a new zone 
         PreviousCamera = CurrentCamera;
