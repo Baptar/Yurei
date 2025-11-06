8191e91832bbbbb98b09c25d27c0b86bb4a61c8c
BRS0T <spam.brst@gmail.com>
2025-11-06 14:00:45 +0100

    Wwise basic architecture without audio

--- Yurei/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Yurei/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,10 +11,61 @@
 
 namespace AK
 {
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ORGANIC = 2934663280U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID SNOW = 787898836U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+        namespace PLAYERCYCLE
+        {
+            static const AkUniqueID GROUP = 1295475536U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID IDLE = 1874288895U;
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace PLAYERCYCLE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_PROTO = 1755220707U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID AMBBUS = 2894416467U;
+        static const AkUniqueID JINGLEBUS = 590867056U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOVABLEOBJECTSBUS = 2112977367U;
+        static const AkUniqueID MUSICBUS = 2886307548U;
+        static const AkUniqueID PUPPETBUS = 1406467327U;
+        static const AkUniqueID RVB = 695384145U;
+        static const AkUniqueID SFXBUS = 3803850708U;
+        static const AkUniqueID UI_MENUSBUS = 2401090920U;
+        static const AkUniqueID UIBUS = 1372881427U;
+        static const AkUniqueID VOICEBUS = 2045367873U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ENORMOUSRVB = 92103823U;
+        static const AkUniqueID HALL = 3633416828U;
+        static const AkUniqueID SMALL_ROOM = 4103918620U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
