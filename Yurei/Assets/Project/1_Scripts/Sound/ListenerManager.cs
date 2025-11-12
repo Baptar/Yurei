95757c456e979bf3e026c1741aa1f0db0df92604
BRS0T <spam.brst@gmail.com>
2025-11-12 17:24:34 +0100

    ListenerManager update (toggle to stick it to the player) + Wwise project update

--- Yurei/Assets/Project/1_Scripts/Sound/ListenerManager.cs
+++ Yurei/Assets/Project/1_Scripts/Sound/ListenerManager.cs
@@ -6,10 +6,34 @@ public class ListenerManager : MonoBehaviour
 {
     public static ListenerManager Instance;
 
-    private void Awake() => Instance = this;
-    
+    [Header("Listener Settings")]
+    [Tooltip("Si activé, le listener reste sur le joueur au lieu de suivre les zones caméra.")]
+    public bool stayOnPlayer = false;
+    private Transform playerTransform;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Trouve le joueur via son tag
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+            playerTransform = player.transform;
+    }
+
+    private void Update()
+    {
+        if (stayOnPlayer && playerTransform != null)
+        {
+            transform.position = playerTransform.position;
+            transform.rotation = playerTransform.rotation;
+        }
+    }
     public void MoveListenerTo(CameraZone zoneCamera)
     {
+        if (stayOnPlayer)
+            return;
+            
         Sequence sequence = DOTween.Sequence();
 
         sequence.Append(gameObject.transform.DOMove(zoneCamera.GetCameraAssociated().transform.position, zoneCamera.timeCameraListener)).SetEase(zoneCamera.easeCameraListener)
